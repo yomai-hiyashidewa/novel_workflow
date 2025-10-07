a109654ae2ccec8813a1f6671477a755aaf4d539
yomai-hiyashidewa <yomai.hiyashidewa.gmail.com>
2025-10-07 21:35:04 +0900

    add args

--- apps/main.py
+++ apps/main.py
@@ -1,10 +1,21 @@
 import sys
 import os
+import argparse
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from apps.core.directer import Director
 from apps.domains.workflow_step import WorkflowStep
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a workflow step.")
+    parser.add_argument(
+        "--step",
+        type=str,
+        choices=[step.value for step in WorkflowStep],
+        default=WorkflowStep.RECORDER.value,
+        help="The workflow step to run."
+    )
+    args = parser.parse_args()
+
     director = Director()
-    director.run(step=WorkflowStep.RECORDER)
+    director.run(step=WorkflowStep(args.step))
